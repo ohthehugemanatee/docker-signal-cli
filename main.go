2c8923d207163b602f03e5205a5cca852ab4ab4a
Campbell Vertesi <campbell@vertesi.com>
2019-09-09 23:42:51 +0200

    Fix phone number variable, put scanning back into a go func

--- main.go
+++ main.go
@@ -94,8 +94,8 @@ func parseFlags() {
 		flag.PrintDefaults()
 	}
 	flag.Parse()
-	MyPhone := *phonePtr
-	TargetGroupID := *groupPtr
+	MyPhone = *phonePtr
+	TargetGroupID = *groupPtr
 	NixplayEmail = *mailPtr
 	MailServer = *MPtr
 	MailUser = *mailUserPtr
@@ -127,23 +127,25 @@ func StartSignal(myPhone string, targetGroupID string, writer io.Writer) (*exec.
 func FilterMessages(stdout io.Reader, targetGroupID string, filenameChannel chan string, writer io.Writer) {
 	scanner := bufio.NewScanner(stdout)
 	scanner.Split(bufio.ScanLines)
-	for scanner.Scan() {
-		text := scanner.Bytes()
-		if text != nil {
-			var message Message
-			json.Unmarshal(text, &message)
-			attachments := message.Envelope.DataMessage.Attachments
-			if message.Envelope.DataMessage.GroupInfo.GroupID == targetGroupID && attachments != nil {
-				for i := 0; i < len(attachments); i++ {
-					if attachments[i].ContentType == "image/jpeg" &&
-						attachments[i].Size > 512000 {
-						filenameChannel <- strconv.Itoa(attachments[i].ID)
-						fmt.Fprintf(writer, "Send attachment id %v", attachments[i].ID)
+	go func() {
+		for scanner.Scan() {
+			text := scanner.Bytes()
+			if text != nil {
+				var message Message
+				json.Unmarshal(text, &message)
+				attachments := message.Envelope.DataMessage.Attachments
+				if message.Envelope.DataMessage.GroupInfo.GroupID == targetGroupID && attachments != nil {
+					for i := 0; i < len(attachments); i++ {
+						if attachments[i].ContentType == "image/jpeg" &&
+							attachments[i].Size > 512000 {
+							filenameChannel <- strconv.Itoa(attachments[i].ID)
+							fmt.Fprintf(writer, "Send attachment id %v", attachments[i].ID)
+						}
 					}
 				}
 			}
 		}
-	}
+	}()
 }
 
 // SendMail using the CLI-defined params.
